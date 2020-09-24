a0a11c9d8909d5f91d458cdb30e351d10bbefeed
chenran <chenran@qq.com>
2020-09-24 14:35:36 +0800

    优化ModelResult

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,7 +5,6 @@ import com.loror.sql.ModelResultList;
 import com.loror.sql.SQLClient;
 import com.loror.sql.mysql.MySQLClient;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -18,7 +17,6 @@ public class Test {
             //日志
             sqlClient.setLogListener((connect, sql) -> {
                 System.out.println(sql + (connect ? "(查询)" : "(缓存)"));
-                singleTable(sql);
             });
 
             //设置缓存
@@ -79,84 +77,4 @@ public class Test {
         }
     }
 
-    private static String singleTable(String baseSql) {
-        if (baseSql == null) {
-            return null;
-        }
-        String sql = baseSql.toLowerCase();
-
-        String table = null;
-
-        int index = -1;
-        while ((index = sql.indexOf("from ", index + 1)) != -1) {
-            int left = index;
-            left += 4;
-            while (sql.charAt(left) == ' ') {
-                left++;
-            }
-
-            if (left == sql.length() - 1) {
-                break;
-            }
-
-            if (sql.charAt(left) == '`') {
-                int right = sql.indexOf("`", left + 1);
-                if (right != -1) {
-                    if (table != null) {
-                        table = null;
-                        break;
-                    }
-                    table = sql.substring(left + 1, right);
-                }
-                continue;
-            }
-
-            int right = sql.indexOf(" on", index);
-            if (right == -1) {
-                right = sql.indexOf(" where", index);
-            }
-            if (right == -1) {
-                right = sql.indexOf(" having", index);
-            }
-            if (right == -1) {
-                right = sql.indexOf(" group by", index);
-            }
-            if (right == -1) {
-                right = sql.indexOf(" limit", index);
-            }
-            String item;
-            if (right != -1) {
-                item = sql.substring(left, right);
-            } else {
-                item = sql.substring(left);
-            }
-            List<String> names = new ArrayList<>();
-
-            while (true) {
-                int itemRight = item.indexOf(" left join");
-                if (itemRight != -1) {
-                    names.add(item.substring(0, itemRight));
-                    item = item.substring(itemRight + 10);
-                }
-                itemRight = item.indexOf(",");
-                if (itemRight != -1) {
-                    names.add(item.substring(0, itemRight));
-                    item = item.substring(itemRight + 1);
-                } else {
-                    names.add(item);
-                    break;
-                }
-            }
-            for (String name : names) {
-                name = name.trim();
-                int nameRight = name.indexOf(" as");
-                if (nameRight != -1) {
-//                    table.add(name.substring(0, nameRight));
-                } else {
-//                    table.add(name);
-                }
-            }
-        }
-        return table;
-    }
 }
